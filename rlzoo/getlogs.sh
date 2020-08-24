ca0eaea9518a0cf20afd72830856799bb47cee66
Rana Adhikari <rana@caltech.edu>
2020-08-24 13:17:56 -0700

    bash script to get training log files from remote training machine * can run periodically: watch -n 15 bash getlogs.sh

---
+++ rlzoo/getlogs.sh
@@ -0,0 +1,3 @@
+rsync -ahP rana.adhikari@ldas-pcdev7.ligo.caltech.edu:GIT/controlsystems/RLzoo/rlzoo/model/* model/
+
+rsync -ahP rana.adhikari@ldas-pcdev7.ligo.caltech.edu:GIT/controlsystems/RLzoo/rlzoo/log/* log/
