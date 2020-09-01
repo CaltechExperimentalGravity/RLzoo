60bde56e06beac9b82c1b6ee1b6b87093b9b41b0
ellagattis <ella@buehner-gattis.com>
2020-09-01 15:21:30 -0700

    change env type to temp_ctrl

--- rlzoo/tempctrl_rlzoo.py
+++ rlzoo/tempctrl_rlzoo.py
@@ -33,7 +33,7 @@ def dozoo(args):
     # EnvName = 'Pendulum-v0'
     
     EnvName = 'TempControl-v0'
-    EnvType = 'classic_control'
+    EnvType = 'temp_ctrl'
 
     # EnvName = 'BipedalWalker-v2'
     # EnvType = 'box2d'
