cacf9307a935c6a5bae3a2ebed79eb4f9edf0b5a
ellagattis <ella@buehner-gattis.com>
2020-09-11 17:55:40 -0700

    add neptune api token

--- rlzoo/tempctrl_rlzoo.py
+++ rlzoo/tempctrl_rlzoo.py
@@ -6,10 +6,10 @@ import gym_tempcontrol
 from time import time # just to have timestamps in the files
 
 #import optuna
-#import neptune
+import neptune
 #from neptunecontrib.monitoring.keras import NeptuneMonitor
-#neptune.init(ellabg/RL)
-#neptune.create_experiment(name='temp_ctrl')
+neptune.init(ellabg/sandbox, api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiY2I1ODBhNzUtYzAzZC00MmMzLTgyOTktNTJkODY5YzY0MjljIn0=")
+neptune.create_experiment(name='temp_ctrl')
 
 import argparse
 #import json
