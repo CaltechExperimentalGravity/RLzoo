07c5fcd21abd1e293ac52177b2334543a05bd906
ellagattis <ella@buehner-gattis.com>
2020-09-16 15:32:08 -0700

    switch env to VacCan

--- rlzoo/tempctrl_rlzoo.py
+++ rlzoo/tempctrl_rlzoo.py
@@ -34,7 +34,7 @@ def dozoo(args):
     # EnvName = 'CartPole-v0'
     # EnvName = 'Pendulum-v0'
     
-    EnvName = 'TempControl-v0'
+    EnvName = 'VacCanC-v0'
     EnvType = 'classic_control'
     # EnvType = 'temp_ctrl'
 
